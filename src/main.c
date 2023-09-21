de6b22e81f97db855be487d7f093061e6e754c90
Carbon225 <carbon225@protonmail.com>
2023-09-21 20:51:28 +0200

    math

--- src/main.c
+++ src/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 #include "pico/stdlib.h"
+#include <hardware/gpio.h>
 
 #include "gyro.h"
 #include "sbus.h"
 #include "motors.h"
 
+#define LED_PIN (PICO_DEFAULT_LED_PIN)
 
 #define CHAN_LV (0)
-#define CHAN_LH (1)
+#define CHAN_LH (3)
 #define CHAN_RV (2)
-#define CHAN_RH (3)
+#define CHAN_RH (1)
 #define CHAN_POT (4)
+
+#define DEG2RAD(x) ((x) * M_PI / 180.f)
+#define RAD2DEG(x) ((x) * 180.f / M_PI)
 
 
 int main()
@@ -18,7 +24,9 @@ int main()
     stdio_init_all();
     motors_init();
     sbus_init();
-    gyro_init(0, 0, GYRO_FS_245DPS, false, false, GYRO_OUT_SEL_LPF1);
+    gyro_init(0b11, 0b11, GYRO_FS_2000DPS, false, false, GYRO_OUT_SEL_LPF1);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
     struct sbus_packet packet;
 
@@ -34,18 +42,32 @@ int main()
         new_gyro = gyro_read(&vel1);
         if (new_gyro)
         {
-            float vel_offset = SBUS_CHAN_TO_F32(packet.channels[CHAN_POT]) * 2.f - 1.f;
+            float vel_offset =
+                (SBUS_CHAN_TO_F32(packet.channels[CHAN_POT]) * 2.f - 1.f) +
+                50.f * (SBUS_CHAN_TO_F32(packet.channels[CHAN_LH]) * 2.f - 1.f);
             vel1 += vel_offset;
-            ang += (vel0 + vel1) * ((1.f / 100.f) * 0.5f);
+            ang += (vel0 + vel1) * ((1.f / 800.f) * 0.5f);
             if (ang > 180.f) ang -= 360.f;
             else if (ang < -180.f) ang += 360.f;
             vel0 = vel1;
         }
 
-        uint8_t ml = SBUS_CHAN_TO_U8(packet.channels[CHAN_LV]);
-        uint8_t mr = SBUS_CHAN_TO_U8(packet.channels[CHAN_POT]);
+        gpio_put(LED_PIN, -30.f < ang && ang < 30.f);
+
+        float speed_base = SBUS_CHAN_TO_F32(packet.channels[CHAN_LV]);
+        float speed_offset = SBUS_CHAN_TO_F32(packet.channels[CHAN_RV]) * 2.f - 1.f;
+
+        float left_offset = speed_offset * (cosf(DEG2RAD(ang)) + 1.f) * 0.5f;
+        float speed_left = speed_base * (1.f - left_offset);
+
+        float right_offset = speed_offset * (-cosf(DEG2RAD(ang)) + 1.f) * 0.5f;
+        float speed_right = speed_base * (1.f - right_offset);
+
+        uint8_t ml = (uint8_t) (speed_left * 255.f);
+        uint8_t mr = (uint8_t) (speed_right * 255.f);
         motors_set(ml, mr);
 
+        #ifdef DEBUG
         if (timestep % 10000 == 0)
         {
             printf("LV: %d\tLH: %d\tRV: %d\tRH: %d\tPOT: %d\tfailsafe: %d\tframe_lost: %d\n",
@@ -61,6 +83,7 @@ int main()
             printf("ml: %d\tmr: %d\n", ml, mr);
             printf("\n");
         }
+        #endif
     }
 
     return 0;
